6198a6562d35fd9404e2d7c4d2c26d9457a5f94b
tosinfarai <tosinfarai@gmail.com>
2020-07-07 16:07:32 +0100

    initial commit

---
+++ src/main/java/com/example/opentweet/OpenTweetApplication.java
@@ -0,0 +1,13 @@
+package com.example.opentweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenTweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/opentweet/OpenTweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opentweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenTweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

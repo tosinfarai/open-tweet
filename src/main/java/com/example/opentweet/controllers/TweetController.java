7713ba626b4ad4fe38da1d6f12cf04936a6ecf23
tosinfarai <tosinfarai@gmail.com>
2020-07-08 11:56:01 +0100

    added CORS

--- src/main/java/com/example/opentweet/controllers/TweetController.java
+++ src/main/java/com/example/opentweet/controllers/TweetController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.opentweet.entities.Tweet;
 import com.example.opentweet.exceptions.ResourceNotFoundException;
 import com.example.opentweet.repositories.TweetRepository;
 
+@CrossOrigin(origins = {"http://open-tweet-feed.herokuapp.com"})
 @RestController
 @RequestMapping("/feed")
 public class TweetController {

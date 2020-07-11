836868046aa05d90eec2f6dc06c0e3d195946153
tosinfarai <tosinfarai@gmail.com>
2020-07-11 17:21:33 +0100

    date and time added

--- src/main/java/com/example/opentweet/entities/Tweet.java
+++ src/main/java/com/example/opentweet/entities/Tweet.java
@@ -1,5 +1,7 @@
 package com.example.opentweet.entities;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table (name = "tweets")
@@ -16,11 +20,12 @@ public class Tweet {
 		
 	}
 	
-	public Tweet(Long id, String name, String tweet) {
+	public Tweet(Long id, String name, String tweet, LocalDateTime dateTime) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.tweet = tweet;
+		this.dateTime = dateTime;
 	}
 	
 
@@ -32,8 +37,12 @@ public class Tweet {
 	private String name;
 	
 	@Column (name = "tweet", nullable = false)
-	@Size (max = 280, message = "What's happening?")
+	@Size (max = 280, message = "Cannot exceed 280 characters")
 	private String tweet;
+	
+	@Column(name = "date_time", updatable=false)
+	@CreationTimestamp
+	private LocalDateTime dateTime;
 
 	
 	public long getId() {
@@ -58,6 +67,14 @@ public class Tweet {
 
 	public void setTweet(String tweet) {
 		this.tweet = tweet;
+	}
+	
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
 	}
 
 }
